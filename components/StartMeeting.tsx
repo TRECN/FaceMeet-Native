7abc57e3f3d6833fd807827eee875ea34693ce79
TRECN <rishabh1103@outlook.com>
2023-05-29 03:42:12 +0530

    added conferenceCall

--- components/StartMeeting.tsx
+++ components/StartMeeting.tsx
@@ -1,21 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import ZegoUIKitPrebuiltVideoConference from '@zegocloud/zego-uikit-prebuilt-video-conference-rn'
 type Props = {
   UserName:string,
   UserId:string,
   RoomName:string,
   RoomId:string
 }
+const AppID=978224315
+const AppSign='1926c8f0a97957174f61c153eb39725c74da9c7dd7f8397bc84e25c64ef1d870'
 
-const StartMeeting = ({UserName,UserId,RoomName,RoomId}: Props|any) => {
+const StartMeeting = ({UserName,UserId,RoomName,RoomId,navigation}: Props|any) => {
   return (
-    <View>
+    <View style={styles.container}>
       
-    </View>
+    <ZegoUIKitPrebuiltVideoConference
+        appID={AppID}
+        appSign={AppSign}
+        userID={UserId} // userID can be something like a phone number or the user id on your own user system. 
+        userName={UserName}
+        conferenceID={RoomName} // conferenceID can be any unique string. 
+
+        config={{
+            onLeave: () => { navigation.navigate('MainScreen') },
+        }}
+    />
+</View>
   )
 }
 
 export default StartMeeting
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    height:'100%'
+  }
+})
