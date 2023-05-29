100f55e2869df04a1321fd468950b68100a2186e
TRECN <rishabh1103@outlook.com>
2023-05-30 04:37:07 +0530

    adding routes to main server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser= require('body-parser')
 const DbConnect = require('./config/db')
+const user = require('./routes/user')
 
 DbConnect()
 const app = express()
@@ -12,6 +13,8 @@ app.use(bodyParser.json())
 app.get('/',(req,res)=>{
     res.json({message:'API WORKING!!'})
 })
+
+app.use('/user',user)
 
 app.listen(PORT,(req,res)=>{
     console.log(`server started at port: ${PORT}`)
